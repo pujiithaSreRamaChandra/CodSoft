f6865c923cc3bce8be763cf80626e35857c64a94
PujiithaSreCharapaka <147905354+pujiithaSreRamaChandra@users.noreply.github.com>
2024-06-14 10:48:56 +0530

    Add files via upload

---
+++ Tribute-Page/files/Tribute.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "50px";
+    document.getElementById("openbtn").style.display = "none";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("openbtn").style.display = "block"
+}
